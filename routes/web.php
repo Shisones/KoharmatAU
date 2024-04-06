ed4fa65d519f511a78fcc1ec60c84b47dc8bb96b
FRQNC <alhaqqi123@gmail.com>
2024-04-06 11:41:37 +0700

    Added routing and also files for the pages: beranda, tentangKami, informasi, galeri, kontak

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index',['title' => 'Beranda']);
+});
+
+Route::get('/tentangkami', function () {
+    return view('tentangKami',['title' => 'Tentang Kami']);
+});
+
+Route::get('/informasi', function () {
+    return view('informasi',['title' => 'Informasi']);
+});
+
+Route::get('/galeri', function () {
+    return view('galeri',['title' => 'Galeri']);
+});
+
+Route::get('/kontak', function () {
+    return view('kontak',['title' => 'Kontak']);
 });
