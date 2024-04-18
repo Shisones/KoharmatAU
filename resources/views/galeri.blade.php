b5d10754d582cc39ec566c15994beb5aac4116c6
tatxha <tatthamaharany@gmail.com>
2024-04-18 12:12:00 +0700

    template for galeri page is almost done. the filter it doesnt work. i cant fixed the filtering by "foto" or "video"

--- resources/views/galeri.blade.php
+++ resources/views/galeri.blade.php
@@ -22,102 +22,63 @@
             </div>
           </div>
   
-          <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
-  
-            <div class="col-lg-4 col-md-6 portfolio-item filter-foto">
+          <div class="row portfolio-containergaleri d-flex justify-content-center" data-aos="fade-up" data-aos-delay="200" style="align-content: center;">
+            
+            <div class="col-lg-4 col-md-6 portfolio-item filter-foto" style="width: 300px; height: 300px;">
               <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>App 1</h4>
-                <p>App</p>
-                <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+              <div class="details-container">
+                <h2>Nama</h2>
+                <p>Tanggal</p>
               </div>
             </div>
   
-            <div class="col-lg-4 col-md-6 portfolio-item filter-video">
-                <video  controls>
+            <div class="col-lg-4 col-md-6 portfolio-item filter-foto" style="width: 300px; height: 300px;">
+              <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+              <div class="details-container">
+                <h2>Nama</h2>
+                <p>Tanggal</p>
+              </div>
+            </div>
+  
+            <div class="col-lg-4 col-md-6 portfolio-item filter-foto" style="width: 300px; height: 300px;">
+              <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+              <div class="details-container">
+                <h2>Nama</h2>
+                <p>Tanggal</p>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-video" style="width: 300px; height: 300px;">
+                <video controls>
                     <source src="assets/video/1.mp4" type="video/mp4">
                 </video>
-              {{-- <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt=""> --}}
-              <div class="portfolio-info">
-                <h4>Web 3</h4>
-                <p>Web</p>
-                <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-              </div>
+                <div class="details-container">
+                    <h2>Nama</h2>
+                    <p>Tanggal</p>
+                </div>
             </div>
-  
-            <div class="col-lg-4 col-md-6 portfolio-item filter-foto">
-              <img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>App 2</h4>
-                <p>App</p>
-                <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 2"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-              </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-video" style="width: 300px; height: 300px;">
+                <video controls>
+                    <source src="assets/video/2.mp4" type="video/mp4">
+                </video>
+                <div class="details-container">
+                    <h2>Nama</h2>
+                    <p>Tanggal</p>
+                </div>
             </div>
-  
-            <div class="col-lg-4 col-md-6 portfolio-item filter-foto">
-              <img src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Card 2</h4>
-                <p>Card</p>
-                <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 2"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-              </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-video" style="width: 300px; height: 300px;">
+                <video controls>
+                    <source src="assets/video/3.mp4" type="video/mp4">
+                </video>
+                <div class="details-container">
+                    <h2>Nama</h2>
+                    <p>Tanggal</p>
+                </div>
             </div>
-  
-            <div class="col-lg-4 col-md-6 portfolio-item filter-foto">
-              <img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Web 2</h4>
-                <p>Web</p>
-                <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 2"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-              </div>
-            </div>
-  
-            <div class="col-lg-4 col-md-6 portfolio-item filter-foto">
-              <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>App 3</h4>
-                <p>App</p>
-                <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 3"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-              </div>
-            </div>
-  
-            <div class="col-lg-4 col-md-6 portfolio-item filter-foto">
-              <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Card 1</h4>
-                <p>Card</p>
-                <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 1"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-              </div>
-            </div>
-  
-            <div class="col-lg-4 col-md-6 portfolio-item filter-foto">
-              <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Card 3</h4>
-                <p>Card</p>
-                <a href="assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 3"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-              </div>
-            </div>
-  
-            <div class="col-lg-4 col-md-6 portfolio-item filter-foto">
-              <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Web 3</h4>
-                <p>Web</p>
-                <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-              </div>
-            </div>
-  
           </div>
+            
   
         </div>
       </section><!-- End Portfolio Section -->
