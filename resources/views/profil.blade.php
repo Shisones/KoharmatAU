ec3a6789b080c6d1027dfc71358e18948081c03d
tatxha <tatthamaharany@gmail.com>
2024-05-02 23:21:28 +0700

    edited profile.blade style

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -8,11 +8,10 @@
         <div class="container" data-aos="fade-up">
   
           <div class="section-title">
-            <h2>Tentang Kami</h2>
             <h3>Tentang <span>KoharmatAU</span></h3>
             <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
           </div>
-          <div class="row" data-aos="fade-up" data-aos-delay="100">
+          <div class="row py-4" data-aos="fade-up" data-aos-delay="100">
             <div class="col-lg-12 d-flex justify-content-center">
                 <ul id="about-flters">
                     <li data-filter="*" class="filter-active">All</li>
@@ -23,10 +22,10 @@
             </div>
         </div>
         <!-- About section one-->
-        <section class="py-5 bg-light" id="about">
+        <section class="detail py-4 bg-light">
           <div class="container px-5 my-50">
               <div class="row gx-5 align-items-center">
-                  <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
+                  <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://dummyimage.com/500x500/343a40/6c757d" alt="..." /></div>
                   <div class="col-lg-6">
                       <h2 class="fw-bolder">Sejarah Singkat</h2>
                       <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
@@ -35,10 +34,10 @@
           </div>
         </section>
         <!-- About section two-->
-        <section class="py-5" id="about">
+        <section class="detail py-5">
             <div class="container px-5 my-50">
                 <div class="row gx-5 align-items-center">
-                    <div class="col-lg-6 order-first order-lg-last"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
+                    <div class="col-lg-6 order-first order-lg-last"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://dummyimage.com/500x500/343a40/6c757d" alt="..." /></div>
                     <div class="col-lg-6">
                         <h2 class="fw-bolder">Visi &amp; Misi</h2>
                         <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
@@ -47,10 +46,10 @@
             </div>
         </section>
         <!-- About section three-->
-        <section class="py-5 bg-light" id="about">
+        <section class="detail py-5 bg-light">
           <div class="container px-5 my-50">
               <div class="row gx-5 align-items-center">
-                  <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
+                  <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://dummyimage.com/500x500/343a40/6c757d" alt="..." /></div>
                   <div class="col-lg-6">
                       <h2 class="fw-bolder">Tugas</h2>
                       <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
